aae90996d9599063ef002994b67e6dfc665ff41d
indiamaowei7431 <ebzicorn001@gmail.com>
2019-08-30 12:00:58 -0500

    made story, saved as strings

--- app/src/main/java/com/example/ebi_madlibs/InfoSide.java
+++ app/src/main/java/com/example/ebi_madlibs/InfoSide.java
@@ -1,5 +1,6 @@
 package com.example.ebi_madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.RandomAccess;
 
 public class InfoSide extends AppCompatActivity {
+    public static final String MY_NAME = "name";
+    public static final String MY_AGE = "age";
+    public static final String MY_NOUN = "noun";
+    public static final String MY_VERB = "verb";
+    public static final String MY_NUM = "num";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +29,21 @@ public class InfoSide extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        Intent intent = getIntent();
+        String myName = intent.getStringExtra(MY_NAME);
+        String myAge = intent.getStringExtra(MY_AGE);
+        String myNoun = intent.getStringExtra(MY_NOUN);
+        String myVerb = intent.getStringExtra(MY_VERB);
+        String myNum = intent.getStringExtra(MY_NUM);
 
-}
+
+        String strToDisplay = "@string/story1" + myName + "@string/story2" + myAge + "@string/story3" + myNum + "@string/story4" + myNoun +
+                "@string/story4" + myName + "@string/story5" + myName + "@string/story6" + myVerb + "@string/story7" +
+                "@string/story8" + myNoun + "@string/story9";
+        TextView str = (TextView) findViewById(R.id.info);
+        str.setText(strToDisplay);
+            }
+        }
+
+
+
